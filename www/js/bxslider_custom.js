719669c97601da510ef481ba4991724be70e48e6
WebHanna <a.krupchak97@gmail.com>
2018-02-23 12:00:40 +0200

    change news

--- www/js/bxslider_custom.js
+++ www/js/bxslider_custom.js
@@ -7,3 +7,45 @@ $("#contact").click(function() {
         scrollTop: $(".section_seven").offset().top
     }, 1000);
 });
+
+$("#services").click(function() {
+    $('html, body').animate({
+        scrollTop: $(".section_two").offset().top
+    }, 1000);
+});
+
+$("#about_us").click(function() {
+    $('html, body').animate({
+        scrollTop: $(".section_four").offset().top
+    }, 1000);
+});
+
+$("#news").click(function() {
+    $('html, body').animate({
+        scrollTop: $(".section_six").offset().top
+    }, 1000);
+});
+
+$("#contact_sm").click(function() {
+    $('html, body').animate({
+        scrollTop: $(".section_seven").offset().top
+    }, 1000);
+});
+
+$("#services_sm").click(function() {
+    $('html, body').animate({
+        scrollTop: $(".section_two").offset().top
+    }, 1000);
+});
+
+$("#about_us_sm").click(function() {
+    $('html, body').animate({
+        scrollTop: $(".section_four").offset().top
+    }, 1000);
+});
+
+$("#news_sm").click(function() {
+    $('html, body').animate({
+        scrollTop: $(".section_six").offset().top
+    }, 1000);
+});
